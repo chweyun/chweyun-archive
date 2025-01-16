e0d1e85500daa942f2b9c4137d683c4807baea6b
chweyun <chweyun@gmail.com>
2025-01-16 16:58:37 +0900

    style: 홈화면 styling 수정
    - 사진이 세로 중앙 정렬되도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,18 @@ import React from "react";
 export default function Home() {
     return (
         <div>
-            <h1 className="fixed text-6xl text-center mt-40 ml-[8%] font-bold leading-[70px]">
+            <h1
+                className="fixed text-center mt-40 ml-[8%] font-bold z-10"
+                style={{
+                    fontSize: "6vw",
+                    lineHeight: "7vw",
+                }}
+            >
                 CHWEYUN
                 <br />
                 ARCHIVE
             </h1>
-            <Image src={"/favicon.png"} width="1180" height="808" alt="rock" className="z-50 mx-auto" />
+            <Image src={"/favicon.png"} width="1180" height="808" alt="rock" className="z-0 mx-auto absolute top-1/2 transform -translate-y-1/2" />
         </div>
     );
 }
