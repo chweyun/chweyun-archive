21ffbf491dbf0f3ce6166a12b0c0a26c839f61c7
chweyun <chweyun@gmail.com>
2025-01-16 14:06:38 +0900

    style: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import {Metadata} from "next";
 import Layout from "@/components/layout/Layout";
 import { baseDomain, blogDesc, blogName, blogThumbnailURL } from '@/config/const';
 import "tailwindcss/tailwind.css";
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKr = Noto_Sans_KR({
+    subsets: ["latin"],
+    weight: ["100", "400", "700", "900"],
+});
 
 export const metadata: Metadata = {
     metadataBase: new URL(baseDomain),
@@ -31,12 +37,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full scroll-my-20 scroll-smooth">
+      <html lang="en" className={`${notoSansKr.className} h-full scroll-my-20 scroll-smooth`}>
       <body className="bg-stone-100 flex min-h-screen flex-col">
       <Layout>
           {children}
       </Layout>
       </body>
-    </html>
-  );
+      </html>
+);
 }
