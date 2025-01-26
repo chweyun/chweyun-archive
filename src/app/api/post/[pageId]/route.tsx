c39f9175c712f3833896fc6b98793a84677d37e3
chweyun <chweyun@gmail.com>
2025-01-26 13:04:04 +0900

    build: 타입 에러 해결

--- src/app/api/post/[pageId]/route.tsx
+++ src/app/api/post/[pageId]/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getPostDetail } from "@/lib/post";
 
 export async function GET(request: Request, { params }: { params: { pageId: string } }) {
-    const { pageId } = await params;
+    const { pageId } = params;
 
     const postDetail = await getPostDetail(pageId);
     return NextResponse.json(postDetail);
